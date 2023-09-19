5d5e8e4633cdb4d1936002995694482b85d58e7d
Jeremy <jeremy.perrouault@ascadis.fr>
2023-09-19 12:17:23 +0200

    JOUR 1 - MVC PATH VARIABLE

--- JEE/PROJETS/formation-jee/src/main/java/fr/formation/controller/HelloController.java
+++ JEE/PROJETS/formation-jee/src/main/java/fr/formation/controller/HelloController.java
@@ -3,6 +3,7 @@ package fr.formation.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.formation.request.HelloFormRequest;
@@ -13,6 +14,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("user", "Jerem");
+
+        return "index";
+    }
+
+    @GetMapping("/hello/{usr}")
+    public String helloPv(@PathVariable(name = "usr") String username, Model model) {
+        model.addAttribute("user", username);
 
         return "index";
     }
