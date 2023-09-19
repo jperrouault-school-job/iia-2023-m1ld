9f3957cf6caefa60e1350cf2225eeacc9d55a73c
Jeremy <jeremy.perrouault@ascadis.fr>
2023-09-19 12:28:07 +0200

    JOUR 1 - VALIDATEURS

--- JEE/PROJETS/formation-jee/src/main/java/fr/formation/request/HelloFormRequest.java
+++ JEE/PROJETS/formation-jee/src/main/java/fr/formation/request/HelloFormRequest.java
@@ -1,10 +1,14 @@
 package fr.formation.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class HelloFormRequest {
+    @NotBlank
     private String username;
+    
+    @NotBlank
     private String password;
 }
