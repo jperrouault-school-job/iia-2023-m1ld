65ba7295af016f746be4fee8999ae23da2573862
Jeremy <jeremy.perrouault@ascadis.fr>
2023-09-19 12:10:45 +0200

    JOUR 1 - INTRO SPRING MVC

---
+++ JEE/PROJETS/formation-jee/src/main/java/fr/formation/controller/HelloController.java
@@ -0,0 +1,33 @@
+package fr.formation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import fr.formation.request.HelloFormRequest;
+
+@Controller // Classe managée par SPRING comme Controller
+public class HelloController {
+    // @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("user", "Jerem");
+
+        return "index";
+    }
+
+    // @PostMapping("/hello")
+    // public String postHello(@RequestParam String username, @RequestParam String password, Model model) {
+    //     model.addAttribute("user", username);
+
+    //     return "index";
+    // }
+
+    @PostMapping("/hello")
+    public String postHello(HelloFormRequest request, Model model) {
+        model.addAttribute("user", request.getUsername());
+
+        return "index";
+    }
+}
---
+++ JEE/PROJETS/formation-jee/src/main/java/fr/formation/request/HelloFormRequest.java
@@ -0,0 +1,10 @@
+package fr.formation.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloFormRequest {
+    private String username;
+    private String password;
+}
