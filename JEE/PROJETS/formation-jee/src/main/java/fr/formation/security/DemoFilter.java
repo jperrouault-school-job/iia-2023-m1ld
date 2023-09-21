3e0094803d33829fe5f4f4b85a733b73b7dc8a94
Jeremy <jeremy.perrouault@ascadis.fr>
2023-09-21 10:47:17 +0200

    JOUR 3 - JPA USER & SUBSCRIPTION

--- JEE/PROJETS/formation-jee/src/main/java/fr/formation/security/DemoFilter.java
+++ JEE/PROJETS/formation-jee/src/main/java/fr/formation/security/DemoFilter.java
@@ -1,7 +1,13 @@
 package fr.formation.security;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,7 +20,14 @@ import jakarta.servlet.http.HttpServletResponse;
 public class DemoFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
         
+        authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        
+        Authentication authentication = new UsernamePasswordAuthenticationToken("username", "password qui sert a rien", authorities);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         System.out.println("PRINCIPE DU FILTRE : Appelé à chaque request ...");
 
         // On passe au filtre suivant ...
