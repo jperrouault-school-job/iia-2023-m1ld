09ae30781fbdd90011f24a84471d30f3579d7188
Jeremy <jeremy.perrouault@ascadis.fr>
2023-09-21 09:42:58 +0200

    JOUR 3 - BCRYPT

--- JEE/PROJETS/formation-jee/src/main/java/fr/formation/security/SecurityConfig.java
+++ JEE/PROJETS/formation-jee/src/main/java/fr/formation/security/SecurityConfig.java
@@ -4,21 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public UserDetailsService inMemory() {
+    public UserDetailsService inMemory(PasswordEncoder passwordEncoder) {
         InMemoryUserDetailsManager inMemoryManager = new InMemoryUserDetailsManager();
+        String encoded = passwordEncoder.encode("123456$");
 
         inMemoryManager.createUser(
             User.withUsername("jeremy")
-                .password("{noop}123456$")
+                .password(encoded)
                 .roles("ADMIN")
                 .build()
         );
 
         return inMemoryManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
